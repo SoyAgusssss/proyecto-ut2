89f878047f2431a4baa8b3657938bd33aa5fbf7b
Miguel_Agustin <miguel-agus1995@hotmail.com>
2025-12-16 21:38:44 +0100

    cambio en el menú

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -9,40 +9,50 @@ declare var bootstrap: any;
 })
 export class MenuComponent implements AfterViewInit {
 
-  @ViewChild('modalInscripcion') modalInscripcion!: ElementRef;
-  @ViewChild('formInscripcion') formInscripcion!: ElementRef;
+  @ViewChild('modalInscripcion', { static: false }) modalInscripcion?: ElementRef;
+  @ViewChild('formInscripcion', { static: false }) formInscripcion?: ElementRef;
 
   modalInstance: any;
 
   ngAfterViewInit(): void {
+
+    if (!this.modalInscripcion || !this.formInscripcion) {
+      return;
+    }
+
     // Crear instancia del modal de Bootstrap
     this.modalInstance = new bootstrap.Modal(this.modalInscripcion.nativeElement);
 
     // Cuando se abre el modal → enfocar primer input
     this.modalInscripcion.nativeElement.addEventListener('shown.bs.modal', () => {
       const primerInput =
-        this.modalInscripcion.nativeElement.querySelector('input,select,textarea');
+        this.modalInscripcion?.nativeElement.querySelector('input,select,textarea');
       if (primerInput) primerInput.focus();
     });
 
     // Cuando se cierra el modal → reset del formulario
     this.modalInscripcion.nativeElement.addEventListener('hidden.bs.modal', () => {
-      this.formInscripcion.nativeElement.reset();
+      this.formInscripcion?.nativeElement.reset();
     });
   }
 
   enviarInscripcion() {
+    if (!this.modalInstance) return;
+
     // Cerrar modal
     this.modalInstance.hide();
 
     // Mostrar toast después de un pequeño retraso
     setTimeout(() => {
       const toastElement = document.getElementById('toastInscripcion');
-      bootstrap.Toast.getOrCreateInstance(toastElement).show();
+      if (toastElement) {
+        bootstrap.Toast.getOrCreateInstance(toastElement).show();
+      }
     }, 300);
 
     // Reset por si acaso
-    this.formInscripcion.nativeElement.reset();
+    this.formInscripcion?.nativeElement.reset();
   }
 }
+
 
